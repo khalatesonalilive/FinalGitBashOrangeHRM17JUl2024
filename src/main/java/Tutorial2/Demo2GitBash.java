ef7a5a3610a42068206320c63dc2875303f64c76
khalatesonalilive <khalatesonali1234@gmail.com>
2024-07-17 23:14:36 +0530

    HomePage Test Cases was written By Swati

---
+++ src/main/java/Tutorial2/Demo2GitBash.java
@@ -0,0 +1,11 @@
+package Tutorial2;
+
+public class Demo2GitBash {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HomePage Test Cases Written by Swati Automation Tester");
+
+	}
+
+}
